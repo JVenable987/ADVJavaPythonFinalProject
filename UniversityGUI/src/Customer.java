f45694852bbbb7a467a830c8d0206f813f10e0a5
Carlos V <77943334+valdcarl@users.noreply.github.com>
2021-11-26 14:56:53 -0700

    class Item added, needs Stocker class to complete the constructor/toString, changed price to float
    class ShoppingCart added, has a constructor, but no real methods aside from a getter method
    
    class Customer added, needs completion of addItemToCart method

---
+++ UniversityGUI/src/Customer.java
@@ -0,0 +1,38 @@
+// class Customer, interacts with the ShoppingCart, add items to his/her ShoppingCart
+
+public class Customer {
+    private String userName;
+    private String password;
+    private boolean isStocker;
+    private ShoppingCart shoppingCart;
+
+    // Customer constructor
+    Customer(String userName, String password, boolean isStocker, ShoppingCart shoppingCart) {
+        this.userName = userName;
+        this.password = password;
+        this.isStocker = isStocker;
+        this.shoppingCart = shoppingCart;
+    }
+
+    public String getUserName(){ return userName; }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword(){ return password; }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsStocker() { return isStocker; }
+    public void setIsStocker(boolean isStocker) {
+        this.isStocker = isStocker;
+    }
+
+    public ShoppingCart getShoppingCart() { return shoppingCart; }
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    // addItemToCart(int pid) void
+}
