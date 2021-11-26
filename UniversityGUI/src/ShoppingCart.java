11da38d6ca576f97a421b2f1733e3eaf53e7e8d9
Carlos V <77943334+valdcarl@users.noreply.github.com>
2021-11-26 14:47:51 -0700

    class Item added, needs Stocker class to complete the constructor/toString, changed price to float
    class ShoppingCart added, has a constructor, but no real methods aside from a getter method

---
+++ UniversityGUI/src/ShoppingCart.java
@@ -0,0 +1,21 @@
+// class ShoppingCart
+// this object is composed of Item objects
+
+import edu.cudenver.university.Student;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    // thinking if we should have a cartID? for that unique cart?
+    private ArrayList<Item> cartList;       // list of Items
+
+    //Constructor for ShoppingCart
+    public ShoppingCart(){
+        this.cartList = new ArrayList<>(100);   // assumed to have a capacity of 100 items
+    }
+
+    public ArrayList<Item> getCartList(){ return cartList; }
+
+    // No methods for appending/adding to the cartList added as of yet or toString to check if the items accurately added
+
+}
