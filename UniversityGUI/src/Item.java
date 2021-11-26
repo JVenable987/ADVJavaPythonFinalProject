895c6b846e818fa058e16264adb218b692acccd3
Carlos V <77943334+valdcarl@users.noreply.github.com>
2021-11-26 12:04:25 -0700

    class Item added, needs Stocker class to complete the constructor, & the toString

---
+++ UniversityGUI/src/Item.java
@@ -0,0 +1,61 @@
+// class Item
+
+import edu.cudenver.university.Course;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Item {
+    private int productID;                    // Haven't made const yet (static)
+    //private static Stocker stockerAdded;    // Stocker class not defined yet
+    private String type;
+    private int quantity;
+    private float price;            // Changed it to float, not updated on the UML
+    private String manufacturer;
+    private String model;
+
+    // Constructor for Item     (STILL NEEDS STOCKER AFTER productID)
+    public Item(int productID, String type, int quantity, int price, String manufacturer, String model){
+        this.productID = productID;
+        this.type = type;
+        this.quantity = quantity;
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    public int getProductID(){ return productID; }
+    public void setProductID(int productID){ this.productID = productID; }
+
+    public String getType() { return type; }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() { return price; }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getManufacturer() { return manufacturer; }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() { return model; }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    // NEEDS ToString()
+    public String toString(){
+        return String.format("ID: %d | Type: %s | Quantity: %d | Price: $%.2f | Manufacturer: %s | Model: %s",
+                this.getProductID(), this.getType(), this.getQuantity(), this.getPrice(), this.getManufacturer(), this.getModel());
+    }
+
+}   // end class Item
