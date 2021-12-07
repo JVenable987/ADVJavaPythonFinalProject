a04481d2abf9a495a65a49ec9ffff816960507ac
Carlos V <77943334+valdcarl@users.noreply.github.com>
2021-12-07 13:10:04 -0700

    Update Catalog.java
    Added some methods:
    addItem() : adds a single item object created to the Catalog
    showCatalog() : prints all the items in the catalog
    sizeOfCatalog() : prints the amount of catalog items that exist in the catalog

--- UniversityGUI/src/Catalog.java
+++ UniversityGUI/src/Catalog.java
@@ -1,15 +1,29 @@
-// Items should be added to the Catalog, to essentially make up the store
+// Catalog contains all the Items in the system
 
 import java.util.ArrayList;
 
 public class Catalog {
-    private ArrayList<Item> items;       // the catalog is composed of various items
+    private ArrayList<Item> items;
 
     public Catalog(){
         this.items = new ArrayList<>();
     }
 
-    public ArrayList<Item> getItemsInCatalog() { return items; }
-    public void setItemsInCatalog(ArrayList<Item> items) { this.items = items; }
+    public ArrayList<Item> getItems() { return items; }
+    public void setItems(ArrayList<Item> items) { this.items = items; }
 
-}   // end class Catalog
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void showCatalog(){
+        // to show everything that is in the catalog (should be all items)
+        for (Item i: items){
+            System.out.println(i);
+        }
+    }
+
+    public void sizeOfCatalog(){
+        System.out.println("Catalog has: " + items.size() + " items");
+    }
+}
