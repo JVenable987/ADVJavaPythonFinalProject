b19ffea887cd8f14e34fa5f50aa4f0f26af852df
JVenable987 <joshua.j.venable@gmail.com>
2021-12-08 16:30:25 -0700

    Update ShoppingCart.py

---
+++ PythonServer/FinalPythonShop/src/ShoppingCart.py
@@ -0,0 +1,19 @@
+#// class ShoppingCart
+#// this object is composed of Item objects
+
+#import edu.cudenver.university.Student;
+
+#import java.util.ArrayList;
+
+class ShoppingCart:
+    #// thinking if we should have a cartID? for that unique cart?
+    cartList = list()       #// list of Items
+
+    #//Constructor for ShoppingCart
+    def __init__(self):
+        self.cartList = list   #// assumed to have a capacity of 100 items
+
+    def getCartList(self):
+        return self.cartList
+
+    #// No methods for appending/adding to the cartList added as of yet or toString to check if the items accurately added
