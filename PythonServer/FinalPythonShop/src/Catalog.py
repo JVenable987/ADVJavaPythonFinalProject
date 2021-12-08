0a9846b1ed05bd61bae8587b5a1affd6cce8d687
Carlos V <77943334+valdcarl@users.noreply.github.com>
2021-12-08 16:53:05 -0700

    Updated Catalog.py

--- PythonServer/FinalPythonShop/src/Catalog.py
+++ PythonServer/FinalPythonShop/src/Catalog.py
@@ -1,27 +1,29 @@
-#// Catalog contains all the Items in the system
+# Catalog contains all the items in the system
 
-#import java.util.ArrayList;
 import Item
+
 
 class Catalog:
     items = None
-
+    
     def __init__(self):
-        self.items = list();
-
-    def getItems(self):
-        return self.items
-
-    def setItems(self, items:list):
-        self.items = items
-
-    def addItem(self , item:Item):
-        self.items.append(item)
-
-    def showCatalog(self):
-        # to show everything that is in the catalog (should be all items)
-        for i in self.items:
+        self._items = list()
+        
+    @property
+    def items(self):
+        return self._items
+    
+    @items.setter
+    def items(self, items):
+        self._items = items
+        
+    def add_item(self, item: Item):
+        self._items.append(item)
+        
+    def show_catalog(self):
+        for i in self._items:
             print(i)
-
-    def sizeOfCatalog(self):
-        print("Catalog has: " + len(self.items) + " items")
+            
+    def size_of_catalog(self):
+        return "Catalog has: {} items".format(len(self._items)) 
+    
